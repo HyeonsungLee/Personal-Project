72653b3048d65dace67256d1eb80f0c49bada6d8
Hyeonsung-Lee <sss23236@naver.com>
2024-11-19 00:31:31 +0900

    Register question

--- src/main/java/lhs/qna/answer/AnswerController.java
+++ src/main/java/lhs/qna/answer/AnswerController.java
@@ -1,10 +1,12 @@
 package lhs.qna.answer;
 
+import jakarta.validation.Valid;
 import lhs.qna.question.Question;
 import lhs.qna.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,14 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = questionService.getQuestion(id);
-        answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
