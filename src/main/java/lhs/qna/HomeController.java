986d2d095336814cdbc58c5bb777e9e9c3e628b7
Hyeonsung-Lee <sss23236@naver.com>
2024-11-19 02:41:18 +0900

    user, HomeController

---
+++ src/main/java/lhs/qna/HomeController.java
@@ -0,0 +1,13 @@
+package lhs.qna;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+}
---
+++ src/main/java/lhs/qna/user/UserRepository.java
@@ -0,0 +1,6 @@
+package lhs.qna.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+}
